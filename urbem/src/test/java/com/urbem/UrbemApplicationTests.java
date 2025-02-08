1b55c1365375a89b03a14cd4e1c8813af1b64c85
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2025-02-08 12:00:17 -0300

    initial project

---
+++ urbem/src/test/java/com/urbem/UrbemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
